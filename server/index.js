171bb8ce75af42785ffecbd844b829934a3eb492
Ivan <ivan.lakhanin@gmail.com>
2021-01-16 15:46:52 +0200

    connecting to database

--- server/index.js
+++ server/index.js
@@ -1,4 +1,21 @@
 const express = require('express')
 const bodyParser = require ('body-parser')
-const mongoose = require ('mogoose')
+const mongoose = require ('mongoose')
 const cors = require ('cors')
+
+const app = express()
+
+app.use(bodyParser.json({limit:'30mb', extended: true}))
+app.use(bodyParser.urlencoded({limit:'30mb', extended: true}))
+app.use(cors())
+
+
+const CONNECTION_URL = 'mongodb+srv://dream-app:dream-app123@cluster0.ne4t7.mongodb.net/dream?retryWrites=true&w=majority'
+
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)) )
+    .catch((error) => console.log(error.message))
+
+mongoose.set('useFindAndModify', false)
