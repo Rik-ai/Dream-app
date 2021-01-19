b9cb448d511f7187e2bf330ecd251268e1f92839
Ivan <ivan.lakhanin@gmail.com>
2021-01-19 14:46:48 +0200

    Create a structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
 
 const App = () => {
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxWidth ='lg'>
+            <AppBar position='static' color='inherit'>
+                <Typography variant='h2' align='center'>Dreams</Typography>
+                <img src="https://img1.picmix.com/output/stamp/normal/2/8/7/0/760782_52476.png" alt='dreams' height='60' />
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify='space-between' alignItems='stretch' spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     )
 }
 
---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,16 @@
+import { Grid } from '@material-ui/core'
+import React from 'react'
+import Post from './Post/Post'
+
+const Posts = () => {
+    return (
+        <>
+            <h1>Posts</h1>
+            <Post/>
+            <Post/>
+
+        </>
+    )
+}
+
+export default Posts
