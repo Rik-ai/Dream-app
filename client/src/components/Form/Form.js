0a605fc6edbc4b837990c4db5e4a613b64426b81
Ivan <ivan.lakhanin@gmail.com>
2021-01-19 20:44:37 +0200

    Update posts

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -23,9 +23,12 @@ const Form = ({currentId, setCurrentId}) => {
         } else {
             dispatch(createPost(postData))
         }
+        
+        clear()
     }
     const clear = () => {
-
+        setCurrentId(null)
+        setPostData({creator: '', title: '', message: '', tags: '', selectedFile: ''})
     }
 
   
