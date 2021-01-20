2cf73ff6be78ad36b3331985b92eeb38f173b9fa
Ivan <ivan.lakhanin@gmail.com>
2021-01-20 15:47:32 +0200

    Fix code and add .env

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const bodyParser = require ('body-parser')
 const mongoose = require ('mongoose')
 const cors = require ('cors')
+const dotenv = require('dotenv')
 
 const postRoutes = require('./routes/posts')
 
 const app = express()
+dotenv.config()
 
 app.use(bodyParser.json({limit:'30mb', extended: true}))
 app.use(bodyParser.urlencoded({limit:'30mb', extended: true}))
@@ -13,11 +15,11 @@ app.use(cors())
 
 app.use('/posts', postRoutes)
 
-const CONNECTION_URL = 'mongodb+srv://dream-app:dream-app123@cluster0.ne4t7.mongodb.net/dream?retryWrites=true&w=majority'
+// const CONNECTION_URL = 'mongodb+srv://dream-app:dream-app123@cluster0.ne4t7.mongodb.net/dream?retryWrites=true&w=majority'
 
 const PORT = process.env.PORT || 5000
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)) )
     .catch((error) => console.log(error.message))
 
